b4a5e7d9cf83e1c46f616f01af30b60fe5f36b34
David Stinghe <dstinghe@3-F-1.42heilbronn.de>
2024-07-25 04:17:31 +0200

    add mem alloc and mutex init functions; update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: castronela <castronela@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dstinghe <dstinghe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:25:44 by castronela        #+#    #+#             */
-/*   Updated: 2024/07/04 14:41:14 by castronela       ###   ########.fr       */
+/*   Updated: 2024/07/25 04:16:45 by dstinghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,29 @@
 int	main(int ac, char **av)
 {
 	int i;
-	long long num;
 	data_t data;
 
 	if (ac == 5 || ac == 6)
 	{
 		i = 0;
-		data.eat_counter = -1;
 		while (++i < ac)
 		{
-			if (input_valid(av[i]) == false)
-				return (input_error());
-			num = ft_atoi(av[i]);
-			if (num > INT_MAX)
-				return (input_error());
-			init_data(&data, i, (int)num);
+			if (input_valid(av[i]) == false || ft_atoi(av[i]) > INT_MAX)
+				return (error(&data, ERROR_INPUT, i));
+			if (i == 1)
+			{
+				data.philo_total = ft_atoi(av[i]);
+				if (data.philo_total == 0)
+					return (error(&data, ERROR_INPUT, i));
+				if (setup_data(&data))
+					return (1);
+			}
+			else
+				init_input_data(&data, i, (int)ft_atoi(av[i]));
 		}
 		return (launch_threads(&data));		
 	}
-	return (1);
+	return (error(&data, ERROR_INPUT, 0));
 }
 
 // printf("p_count: %d\ntime_death: %d\ntime_eat: %d\ntime_sleep: %d\neat_count: %d\n", data.philo_count, data.time_death, data.time_eat, data.time_sleep, data.eat_counter);
--- utils.c
+++ utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: castronela <castronela@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dstinghe <dstinghe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 13:50:38 by castronela        #+#    #+#             */
-/*   Updated: 2024/07/04 14:16:24 by castronela       ###   ########.fr       */
+/*   Updated: 2024/07/25 04:11:12 by dstinghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,16 @@ bool input_valid(char *av)
 	return (true);
 }
 
-int input_error(void)
+int error(t_data_t *data, char *err_msg, int clean_all)
 {
-	printf(INPUT_ERROR);
+	if (clean_all > 1)
+	{
+		mutex_destroy(data->lock_fork, data->phio_total);
+		mutex_destroy(data->lock_thread, data->phio_total);
+		pthread_mutex_destroy(&data->lock_printf);
+		clean(data);
+	}
+	printf(err_msg);
 	return (1);
 }
 
@@ -78,16 +85,197 @@ long long ft_atoi(char *av)
     return (num);    
 }
 
-void init_data(data_t *data, int ac, int value)
+int ft_strncmp(char *s1, char *s2, int n)
 {
-    if (ac == 1)
-        data->philo_count = value;
-    else if (ac == 2)
-        data->time_death = value;
-    else if (ac == 3)
-        data->time_eat = value;
-    else if (ac == 4)
-        data->time_sleep = value;
-    else if (ac == 5)
-        data->eat_counter = value;
+	if (!s1 || !s2)
+		return (0);
+	while (*s1 && *s2 && *s == *s2 && n)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	return (*s1 - *s2);
+}
+
+void init_input_data(t_data_t *data, int ac, int value)
+{
+	int i;
+
+	data->time_synch_start += SYNCH_DELAY_U_SEC;
+	i = -1;
+	while (++i < data->philo_total)
+	{
+		if (ac == 2)
+		{
+			data->philo[i].time_death = value;
+			init_data(data, i);	
+		}
+		else if (ac == 3)
+			data->philo[i].time_eat = value;
+		else if (ac == 4)
+			data->philo[i].time_sleep = value;
+		else if (ac == 5)
+			data->philo[i].eat_count = value;		
+	}
+}
+
+void init_data(t_data_t *data, int i)
+{
+	data->philo[i].time_synch_start = data->time_synch_start;
+	data->philo[i].philo_total = data->philo_total;
+	data->philo[i].philo_id = i + 1;
+	data->philo[i].fork_count = 0;
+	data->philo[i].status = START;
+	data->philo[i].time = 0;
+	data->philo[i].time_event = 0;
+	data->philo[i].time_eat_last = 0;
+	data->philo[i].eat_count = -1;
+	data->philo[i].lock_printf = &data->lock_printf;
+	
+	data->fork[i] = FORK;
+	data->threa_status[i] = RUNNING;
+}
+
+int setup_data(t_data_t *data)
+{
+	if (memalloc_data(data))
+		return (error(data, ERROR_MEM_ALLOC, 0));
+	if (init_time(&data->time_synch_start))
+		return (error(data, GETTIMEOFDAY, 2));
+	if (init_mutex(data))
+		return (error(data, ERROR_MUTEX_INIT, 2));
+	return (0);
+}
+
+int memalloc_data(t_data_t *data)
+{
+	data->philo = malloc(data->philo_total * sizeof(t_philo_t));
+	if (data->philo == NULL)
+		return (1);
+	data->philo_thread = malloc(data->philo_total * sizeof(pthread_t));
+	if (data->philo_thread == NULL)		
+	{
+		free (data->philo);
+		return (1);
+	}
+	if (vars_allocmem(data))
+	{
+		free (data->philo_thread);
+		free (data->philo);
+		return (1);		
+	}
+	if (mutex_allocmem(data))
+	{
+		free (data->philo_status);
+		free (data->forks);
+		free (data->philo_thread);
+		free (data->philo);
+		return (1);		
+	}
+	return (0);
+}
+
+int init_time(uint64_t *time)
+{
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL))
+		return (1);
+	*time = (uint64_t) (tv.tv_sec * 1000000) + (uint64_t) tv.tv_usec;;
+	return (0);
+}
+
+int init_mutex(t_data_t *data)
+{
+	int i;
+
+	if (pthread_mutex_init(&data->lock_printf, NULL))
+		return (1);
+	i = -1;
+	while (++i < data->philo_total);
+	{
+		if (pthread_mutex_init(&data->lock_fork[i], NULL))
+		{
+			pthread_mutex_destroy(&data->lock_printf);
+			mutex_destroy(data->lock_fork, i);
+			return (1);
+		}
+		if (pthread_mutex_init(&data>lock_thread[i], NULL))
+		{
+			pthread_mutex_destroy(&data->lock_fork[i], NULL);
+			mutex_destroy(data->lock_fork, i + 1);
+			mutex_destroy(data->lock_thread, i);
+			return (1);			
+		}
+	}
+	return (0);
+}
+
+void mutex_destroy(pthread_mutex_t *lock, int i)
+{
+	while (--i >= 0)
+		pthread_mutex_destroy(&lock[i]);
+}
+
+void clean(t_data_t *data)
+{
+	free(data->lock_thread);
+	free(data->thread_status);
+	free(data->lock_fork);
+	free(data->forks);
+	free(data->philo_thread);
+	free(data->philo);
+}
+
+int vars_allocmem(t_data_t *data)
+{
+	int i;
+
+	data->fork = malloc(data->philo_total * sizeof(t_fork));
+	if (data->fork == NULL)
+		return (1);
+	data->thread_status == malloc(data->philo_total * sizeof(t_thread));
+	if (data->thread_status == NULL)
+	{
+		free(data->fork);
+		return (1);
+	}
+	i = -1;
+	while (++1 < data->philo_total)
+	{
+		data->philo[i].fork_left = &data->fork[i];
+		if (i + 1 == data->philo_total)
+			data->philo[i].fork_right = &data->fork[0];
+		else
+			data->philo[i].fork_right = &data->fork[i + 1];
+		data->philo[i].thread = &data->thread_status[i];
+	}
+	return (0);
+}
+
+int mutex_allocmem(t_data_t *data)
+{
+	int i;
+	data->lock_fork = malloc(data>philo_total * sizeof(pthread_mutex_t));
+	if (data->lock_fork == NULL)
+		return (1);
+	data->lock_thread = malloc(data->philo_total * sizeof(pthread_mutex_t));
+	if (data->lock_thread == NULL)
+	{
+		free(data->lock_fork);
+		return (1);
+	}
+	i = -1;
+	while (++i < data->philo_total)
+	{
+		data->philo[i].lock_fork_left = &data->lock_fork[i];
+		if (i + 1 == data->philo_total)
+			data->philo[i].lock_fork_right = &data->lock_fork[0];
+		else
+			data->philo[i].lock_fork_right = &data->lock_fork[i + 1];
+		data->philo[i].lock_thread = &data->lock_thread[i];
+		data->philo[i].lock_printf = &data->lock_printf;				
+	}
+	return (0);
 }
