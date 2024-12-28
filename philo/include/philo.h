1bb8d9ff4ffa115fcb7c978baa785aec2947d1aa
dstinghe <dstinghe@student.42heilbronn.de>
2024-12-28 21:55:22 +0100

    FEAT: Input validation
    - created function-set to verify user input
    - updated header and Makefile

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 18:12:58 by david             #+#    #+#             */
-/*   Updated: 2024/12/28 18:34:08 by david            ###   ########.fr       */
+/*   Updated: 2024/12/28 21:35:20 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,47 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
+# include <stdint.h>
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+# include <stdbool.h>
+# include <limits.h>
 
-int main(void);
+# define INT_MAX_DIGITS 10
+
+# define COLOR_RED "\033[0;31m"
+# define COLOR_RESET "\033[0m"
+
+# define ERRMSG COLOR_RED"Error: "COLOR_RESET
+# define ERRMSG_ARG_FEW "Too few arguments:"
+# define ERRMSG_ARG_MANY "Too many arguments:"
+# define ERRMSG_NUM_INVALID "Invalid number:"
+# define ERRMSG_NUM_LARGE "Argument size larger than 'int' capacity:"
+# define ERRMSG_NUM_NEG "Number must be positive:"
+
+typedef enum e_invalid_input_type {
+    VALID,
+    ARGS_TOO_FEW,     
+    ARGS_TOO_MANY,   
+    INVALID,
+    TOO_LARGE,
+    NEGATIVE
+}   t_invalid_input_type;
+
+typedef struct s_philo_data {
+    size_t philo_count;
+    size_t time_die;
+    size_t time_eat;
+    size_t time_sleep;
+    size_t eat_count;
+}   t_philo_data;
+
+
+int main(int ac, char **av);
+
+// ---- Input validation -----------------------------------------------------------------
+
+bool is_input_valid(t_philo_data *data, size_t ac, char **av);
 
 # endif
--- philo/init_data.c
+++ philo/init_data.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   init_data.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/28 18:31:01 by david             #+#    #+#             */
-/*   Updated: 2024/12/28 21:03:33 by david            ###   ########.fr       */
+/*   Created: 2024/12/28 20:32:12 by david             #+#    #+#             */
+/*   Updated: 2024/12/28 20:34:50 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
 
-int main(int ac, char **av)
-{
-    t_philo_data data;
-    
-    if (is_input_valid(&data, ac, av) == false)
-        return (EXIT_FAILURE);
-    else
-        printf("Good!\n");
-}
+
+
+
 
