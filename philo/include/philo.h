a7eb09bd3a095c019fc0191b7ad7b2aa5068a9a9
dstinghe <dstinghe@student.42heilbronn.de>
2024-12-28 18:57:16 +0100

    Setup file structure

---
+++ philo/include/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 18:12:58 by david             #+#    #+#             */
+/*   Updated: 2024/12/28 18:34:08 by david            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+int main(void);
+
+# endif
